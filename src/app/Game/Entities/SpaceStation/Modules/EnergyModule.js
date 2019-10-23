94de84f08f64135f3ea0f533ccd179461e828f4e
Adriano Lobo <adrianolobo@pop-os.localdomain>
2019-10-22 22:11:07 -0300

    energy module dinamico

--- src/app/Game/Entities/SpaceStation/Modules/EnergyModule.js
+++ src/app/Game/Entities/SpaceStation/Modules/EnergyModule.js
@@ -1,34 +1,63 @@
 import BaseModule from './BaseModule';
 import gameScale from '../../../Constants/gameScale';
+import ModuleBuilder from './ModuleBuilder';
 
 export default class EnergyModule extends BaseModule {
-  constructor(scene, position) {
+  constructor(scene, position, direction) {
     super(scene);
+    this._direction = direction;
     this._baseModule = this.scene.matter.add
       .sprite(position.x, position.y, 'solar-module')
       .setScale(gameScale);
-    this.solarPanel2 = this.scene.matter.add
-      .sprite(0, 0, 'solar-panel')
-      .setScale(gameScale);
-    this.solarPanel2.setPosition(
-      this._baseModule.x,
-      this._baseModule.y - (this._baseModule.displayHeight / 2) -
-                                                          (this.solarPanel2.displayHeight / 2),
-    );
-    this.solarPanel1 = this.scene.matter.add
-      .sprite(0, 0, 'solar-panel')
-      .setFlipY(true)
-      .setScale(gameScale);
-    this.solarPanel1.setPosition(
-      this._baseModule.x,
-      this._baseModule.y + (this._baseModule.displayHeight / 2) +
-                                                          (this.solarPanel1.displayHeight / 2),
-    );
+    if (direction === 'vertical') {
+      this._baseModule.setAngle(90);
+    }
+
+    this._positionSolarPanels();
     BaseModule.setDefaultCollisionCategories(this._baseModule);
     BaseModule.setDefaultCollisionCategories(this.solarPanel1);
     BaseModule.setDefaultCollisionCategories(this.solarPanel2);
     this._baseModule.__self = this;
     this.solarPanel1.__self = this;
     this.solarPanel2.__self = this;
+  }
+  _positionSolarPanels() {
+    const baseModuleSize = ModuleBuilder.getSize(this._baseModule);
+
+    this.solarPanel2 = this.scene.matter.add
+      .sprite(0, 0, 'solar-panel')
+      .setScale(gameScale);
+    this.solarPanel1 = this.scene.matter.add
+      .sprite(0, 0, 'solar-panel')
+      .setFlipY(true)
+      .setScale(gameScale);
+    if (this._direction === 'vertical') {
+      // baseModuleSize = ModuleBuilder.invertSize(baseModuleSize);
+      this.solarPanel1.setAngle(90);
+      this.solarPanel2.setAngle(90);
+
+      this.solarPanel2.setPosition(
+        this._baseModule.x + (baseModuleSize.height / 2) +
+                                              (this.solarPanel2.displayWidth / 2),
+        this._baseModule.y,
+      );
+      this.solarPanel1.setPosition(
+        this._baseModule.x - (baseModuleSize.height / 2) -
+                                              (this.solarPanel2.displayWidth / 2),
+        this._baseModule.y,
+      );
+      return;
+    }
+    this.solarPanel2.setPosition(
+      this._baseModule.x,
+      this._baseModule.y - (baseModuleSize.height / 2) -
+                                                          (this.solarPanel2.displayHeight / 2),
+    );
+
+    this.solarPanel1.setPosition(
+      this._baseModule.x,
+      this._baseModule.y + (baseModuleSize.height / 2) +
+                                                          (this.solarPanel1.displayHeight / 2),
+    );
   }
 }
--- src/app/Game/Entities/SpaceStation/SpaceStation.js
+++ src/app/Game/Entities/SpaceStation/SpaceStation.js
@@ -12,15 +12,14 @@ export default class SpaceStation {
     this.spaceStation.push(new EnergyModule(scene, {
       x: gameConfig.width / 2,
       y: gameConfig.height / 2,
-    }));
-    this.spaceStation[1] = new SocialModule(scene, this.spaceStation[0], 'left', 'horizontal');
-    this.spaceStation[2] = new SocialModule(scene, this.spaceStation[1], 'left', 'vertical');
-    this.spaceStation[3] = new CargoModule(scene, this.spaceStation[2], 'left', 'vertical');
+    }, 'vertical'));
+    this.spaceStation[1] = new SocialModule(scene, this.spaceStation[0], 'top', 'horizontal');
+    this.spaceStation[2] = new SocialModule(scene, this.spaceStation[1], 'top', 'vertical');
+    this.spaceStation[3] = new CargoModule(scene, this.spaceStation[2], 'top', 'horizontal');
 
-    this.spaceStation[4] = new SocialModule(scene, this.spaceStation[0], 'right', 'horizontal');
-    this.spaceStation[5] = new SocialModule(scene, this.spaceStation[4], 'right', 'horizontal');
-    this.spaceStation[6] = new SocialModule(scene, this.spaceStation[5], 'right', 'vertical');
-    this.spaceStation[7] = new CargoModule(scene, this.spaceStation[6], 'bottom', 'horizontal');
+    this.spaceStation[4] = new SocialModule(scene, this.spaceStation[0], 'bottom', 'horizontal');
+    this.spaceStation[5] = new SocialModule(scene, this.spaceStation[4], 'bottom', 'vertical');
+    this.spaceStation[6] = new CargoModule(scene, this.spaceStation[5], 'bottom', 'horizontal');
   }
   update() {
     return this;
