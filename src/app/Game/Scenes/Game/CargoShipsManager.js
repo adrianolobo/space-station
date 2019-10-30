69a49ff5902e2d8cf75eba24d66ce51434e7cbb2
Adriano Lobo <adrianolobo@pop-os.localdomain>
2019-10-29 22:27:32 -0300

    criando ships fora da tela

--- src/app/Game/Scenes/Game/CargoShipsManager.js
+++ src/app/Game/Scenes/Game/CargoShipsManager.js
@@ -2,10 +2,14 @@ import Phaser from 'phaser';
 import CargoShip from '../../Entities/CargoShip/CargoShip';
 
 export default class CargoShipsManager {
-  constructor(scene) {
+  constructor(scene, spawnTime = { min: 4000, max: 7000 }) {
     this.scene = scene;
     this.ships = [];
     this.activeShip = null;
+    this.spawnTime = spawnTime;
+    const sides = ['top', 'right', 'down', 'left'];
+    this.isCreatingShips = true;
+    this.spawnSequence = Phaser.Utils.Array.Shuffle(sides);
   }
   preload() {
     this.scene.load.image('cargo-ship', '/static/img/cargo-ship.png');
@@ -30,7 +34,15 @@ export default class CargoShipsManager {
       },
     ];
     this.newShip();
-    setInterval(this.newShip.bind(this), 5000);
+    this.spawnShips();
+  }
+  spawnShips() {
+    if (!this.isCreatingShips) return;
+    const timeout = Phaser.Math.Between(this.spawnTime.min, this.spawnTime.max);
+    setTimeout(() => {
+      this.newShip();
+      this.spawnShips();
+    }, timeout);
   }
   newShip() {
     const gameConfig = this.scene.cameras.main;
@@ -47,10 +59,19 @@ export default class CargoShipsManager {
     newShip.goFoward();
   }
   generateStartPosition() {
-    console.log(this);
-    // TODO: criar lógica de geração de positions
+    const currentPosition = this.spawnSequence.shift();
+    this.spawnSequence.push(currentPosition);
+    const sceneWidth = this.scene.cameras.main.width;
+    const sceneHeight = this.scene.cameras.main.height;
+    const spawnMargin = 20;
+    const rnd = Phaser.Math.Between;
+    if (currentPosition === 'top') return { x: rnd(0, sceneWidth), y: -spawnMargin };
+    if (currentPosition === 'right') return { x: sceneWidth + spawnMargin, y: rnd(0, sceneHeight) };
+    if (currentPosition === 'down') return { x: rnd(0, sceneWidth), y: sceneHeight + spawnMargin };
+    if (currentPosition === 'left') return { x: -spawnMargin, y: rnd(0, sceneHeight) };
     return { x: 100, y: 100 };
   }
+
   generateCargoAmount() {
     const cargoChance = Phaser.Math.Between(0, 100);
     const cargoSelected = this.cargosPercentage.find(cargoPercentage =>
